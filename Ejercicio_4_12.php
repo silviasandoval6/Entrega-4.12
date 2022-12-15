8900ca15831417f07fbb5ea3fc2af360782b9291
silviasandoval6 <ssandova22@jaumebalmes.net>
2022-12-15 20:32:17 +0100

    Ejercicio vector pares

---
+++ Ejercicio_4_12.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+
+<meta content="text/html; charset=ISO-8859-1" http-
+equiv="content-type">
+
+<title>ejercicio 4.12</title>
+</head>
+<body>
+<?php
+//Iniciamos bucle y vector, actualizando sus posiciones con los primeros números pares
+for ($i=0;$i<10;$i++)
+$v[$i]=$i*2;
+
+//Listamos los echos para printar los resultados por pantalla
+for ($i=0;$i<10;$i++)
+echo $v[$i]<br>;
+?>
+</body>
+</html>
